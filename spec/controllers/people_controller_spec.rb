d73445fe9ee6d3fc5cf89ad9efef34ec28f26aeb
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-06-08 20:36:08 +0100

    write test for updating people

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe PeopleController do 
-
+	before (:each) do
+ 		@person = FactoryGirl.create(:person)
+	end
 
 #unit test to check if the methods exist
 	it 'GET index' do
@@ -15,5 +17,14 @@ RSpec.describe PeopleController do
 		assert_template :new
 	end
 
+    it 'POST create' do
+     expect {
+      post :create, id: @person, :person => {:firstname => @person.firstname}
+        }.to change(Person, :count).by(1)
+    end
 
+    it "PUT update" do
+    	Person.any_instance.should_receive(:update).with({:firstname => @person.firstname})
+    	put :update, {:id => @person.to_param, :person => {:firstname => @person.firstname}}
+    end
 end
