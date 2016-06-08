f8aed59d5f1be0194ed39da71e404d429a8d448c
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-06-08 20:42:13 +0100

    update passed

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,18 +1,18 @@
 class PeopleController < ApplicationController
-  before_action :set_person, only: [:show, :edit, :update, :destroy]
+    before_action :set_person, only: [:show, :edit, :update, :destroy]
 
   def index
-    @people = Person.all
+      @people = Person.all
   end
 
 
   def new
-    @person = Person.new
+      @person = Person.new
   end
 
   def create
-    @person = Person.new(person_params)
-    respond_to do |format|
+      @person = Person.new(person_params)
+      respond_to do |format|
       if @person.save
         format.html { redirect_to @person, notice: 'Person was successfully created.' }
       else
@@ -25,16 +25,19 @@ class PeopleController < ApplicationController
   end
 
   def update
+      @person = Person.find(params[:id])
+      @person.update(person_params)
+      redirect_to people_path
   end
 
 
   private
     def set_person
-      @person = Person.find(params[:id])
+        @person = Person.find(params[:id])
     end
 
     def person_params
-      params.require(:person).permit(:firstname, :lastname)
+        params.require(:person).permit(:firstname, :lastname)
     end
 
 end
